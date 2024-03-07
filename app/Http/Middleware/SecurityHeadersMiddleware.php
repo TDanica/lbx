b8c1a6ead4c94ed2a767dd7ec3d6bae55b8bc365
Danica Tundjova <tundjova_danica@yahoo.com>
2024-03-07 16:02:44 +0100

    LBX test task

---
+++ app/Http/Middleware/SecurityHeadersMiddleware.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class SecurityHeadersMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        $response->header('X-Content-Type-Options', 'nosniff');
+        $response->header('X-Frame-Options', 'SAMEORIGIN');
+        $response->header('X-XSS-Protection', '1; mode=block');
+        $response->header('Referrer-Policy', 'no-referrer');
+
+        return $response;
+    }
+}
